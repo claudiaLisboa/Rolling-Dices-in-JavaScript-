b95d84dee46fdc8c9bac36ee4507825ddf161db8
Claudia Lisboa <ccalumby@gmail.com>
2022-06-14 20:12:11 -0400

    update scripts js

--- assets/scripts/Dices.js
+++ assets/scripts/Dices.js
@@ -1,52 +1,145 @@
-// class Dices {
+export default class Dices {
 
- export default class Dices {
+    constructor(el) {
+        this._el = el; 
+        this._elBtns = this._el.querySelectorAll('[data-js-btn]');
+        this._elImgs = this._el.querySelectorAll('img');
+        this._elTotal = this._el.querySelector('[data-js-total]');
 
-    //class Dices
-    constructor(el){
-        this._el = el;
-        this.elImgs = this._el.querysSlectorAll('[data-js-dice]');
-        this.elBtn = this._el.querysSlectorAll('[data-js-btn]');
-        this.elTotal = this._el.querysSlectorAll('[data-js-total]');
+        this._nbDices = this._elImgs.length;
 
-        this.nbDices= this.elImgs.length;
+        this._randomsNb = []; // tableau par défaut
 
-        this.index = [1,1];
-
-        this.interval = 50;
+        this._interval = 50;
         this._timer = 1000;
 
         this.init();
 
     }
 
+
     /**
-     * comportamente au lance des dies
+     * Initialise les comportements
      */
+    init() {
+        for(let i = 0; i < this._elBtns.length; i++) {
+            this._elBtns[i].addEventListener('click', function(e){
+                let btnName = e.target.dataset.jsBtn;
 
-    throw(){
-
-        // for (let i = 0; i < array.length; i++) {
-        //     const element = array[i];
-            
-        // }
-
-        let animeDice = setInterval(function () {
-            for (let i = 0; i < this._nbDices; i++){
-                this._index[i] = this.randon();
-                this.elImgs[i].setAtributes('src', './assets/img/dices-${this._index[i]}.phg');
-            }
-            
-        })
-
-
-        setInterval(function() {
-            for(let i = 0; i< this._elImgs.length; i++)
-            {
-                this.elImgs[i].setAtributes('src', './assets/img/dices')
-            }
-            
-        }, this._timer);
+                if (btnName == 'throw') {
+                    this.throw();
+                }
+                else if (btnName == 'reset') {
+                    this.reset();
+                }
+            }.bind(this));  // without bind(this), it can't find 'this' when executing the function throw() becuz at the function throw(), it can't find where 'this' is....
+        }
     }
 
- }
+
+    /**
+     * Comportement au lancé des dés
+     */
+    throw() {
+
+        this.toggleBtns(true);
+
+        /*
+        for (let i = 0; i < this._elBtns.length; i++){
+            this._elBtns[i].disabled = true;
+        }
+        */
+
+        let animeDice = setInterval(function() {
+
+            this.dicesFace();
+            /*
+            for (let i = 0; i < this._elImgs.length; i++){
+
+                this._randomsNb[i] = this.random();
+                this._elImgs[i].setAttribute('src', `./assets/img/dice-${ this._randomsNb[i] }.png`)
+
+            }
+            */
+        }.bind(this), this._interval);
+
+
+        setTimeout(function(){
+            clearInterval(animeDice);
+            this.total();
+            this.toggleBtns(false);
+
+            /*
+            for (let i = 0; i < this._elBtns.length; i++){
+                this._elBtns[i].disabled = false;
+            }
+            */
+
+        }.bind(this), this._timer);
+
+        //setTimeout(juste une fois), setInterval(plusieurs fois)
+    }
+
+
+    /**
+     * Toggle attribut disabled btns
+     * @param {bool} bool 
+     */
+    toggleBtns(bool) {
+        for (let i = 0; i < this._elBtns.length; i++){
+            this._elBtns[i].disabled = bool;
+        }
+    }
+
+    /**
+     * Injecte total 
+     */
+    total(){
+        let total = 0;
+
+        for (let i = 0; i < this._randomsNb.length; i++) {
+            total += this._randomsNb[i];
+        }
+
+        this._elTotal.textContent = total;
+
+    }
+
+
+    /**
+     * Injecte l'image de chaque dé
+     */
+    dicesFace(random = true) {
+
+        for (let i = 0; i < this._nbDices; i++){
+
+            if (random) this._randomsNb[i] = this.random();
+            else this._randomsNb[i] = 1;
+
+            this._elImgs[i].setAttribute('src', `./assets/img/dice-${ this._randomsNb[i] }.png`)
+
+        }
+        
+    }
+
+
+    /**
+     * Reset les dés et le total
+     */
+    reset(){
+
+        this.dicesFace(false);
+        this._elTotal.textContent = '';
+    }
+
+
+    /**
+     * Tire un chiffre random de 1 à 6
+     * @returns 
+     */
+    random() {
+        return Math.ceil(Math.random() * 6);
+    }
+
+    
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,12 @@
+import Dices from './Dices.js';
+
 // Lancer les instances de Dice
+
+let elDices = document.querySelectorAll('[data-js-dices]');
+
+for (let i = 0; i<elDices.length; i++){
+
+    new Dices(elDices[i]);
+
+}
+
